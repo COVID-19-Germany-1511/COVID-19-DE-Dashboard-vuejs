570bef41e73d2220cf039eb584fe7ac6775c32c9
Michael Große <mic.grosse@googlemail.com>
2020-03-22 00:36:37 +0100

    Consolidate chart components and their interface
    There was duplication in the Chart components being named ConfirmedCases
    _and_ needed to be given the confirmed cases as a prop.
    
    Currently, the only prop is whether they are drawn on a log y-axis.

--- src/components/charts/ConfirmedCases.vue
+++ src/components/charts/ConfirmedCases.vue
@@ -1,33 +1,41 @@
 <template>
   <div>
     <h2>Verlauf der Infektionen</h2>
-    <CasesLinear :dates="this.dates" :data-sets="this.dataSets" />
+    <CasesLog
+      v-if="this.logarithmic"
+      :dates="this.dates"
+      :data-sets="this.dataSets"
+    />
+    <CasesLinear v-else :dates="this.dates" :data-sets="this.dataSets" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import CasesLinear from '@/components/charts/CasesLinear';
-import { CaseRecordsByState } from '@/store/RootState';
+import CasesLog from '@/components/charts/CasesLog';
 import { Dataset } from '@/lib/transformations/Dataset';
 import { transformCaseRecordsToDataset } from '@/lib/transformations/transformToDatasets';
 import { getDatasetColorPalette } from '@/lib/colors';
 import { COLORS } from '@/constants';
+import { mixins } from 'vue-class-component';
+import StateMixin from '@/components/stateMixin';
 
 @Component({
-  components: { CasesLinear },
+  components: { CasesLinear, CasesLog },
 })
-export default class ConfirmedCasesLinear extends Vue {
-  @Prop({ required: true })
-  public confrimedData!: CaseRecordsByState;
+export default class ConfirmedCases extends mixins(StateMixin) {
+  @Prop({ required: false, default: false })
+  public logarithmic!: boolean;
 
   public get dates(): string[] {
-    return Object.keys(Object.values(this.confrimedData)[0]);
+    return Object.keys(Object.values(this.rootModule.getters.confirmed)[0]);
   }
 
   public get dataSets(): Dataset[] {
-    const dataSets = transformCaseRecordsToDataset(this.confrimedData);
+    const dataSets = transformCaseRecordsToDataset(
+      this.rootModule.getters.confirmed,
+    );
     let colors: string[];
     if (dataSets.length === 1) {
       colors = [COLORS.confirmed];
--- src/components/charts/Deaths.vue
+++ src/components/charts/Deaths.vue
@@ -1,33 +1,41 @@
 <template>
   <div>
     <h2>Verlauf der Totesfälle</h2>
-    <CasesLinear :dates="this.dates" :data-sets="this.dataSets" />
+    <CasesLog
+      v-if="this.logarithmic"
+      :dates="this.dates"
+      :data-sets="this.dataSets"
+    />
+    <CasesLinear v-else :dates="this.dates" :data-sets="this.dataSets" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import CasesLinear from '@/components/charts/CasesLinear';
-import { CaseRecordsByState } from '@/store/RootState';
 import { Dataset } from '@/lib/transformations/Dataset';
 import { transformCaseRecordsToDataset } from '@/lib/transformations/transformToDatasets';
 import { COLORS } from '@/constants';
 import { getDatasetColorPalette } from '@/lib/colors';
+import CasesLog from '@/components/charts/CasesLog';
+import { mixins } from 'vue-class-component';
+import StateMixin from '@/components/stateMixin';
 
 @Component({
-  components: { CasesLinear },
+  components: { CasesLog, CasesLinear },
 })
-export default class DeathsLinear extends Vue {
-  @Prop({ required: true })
-  public deaths!: CaseRecordsByState;
+export default class Deaths extends mixins(StateMixin) {
+  @Prop({ required: false, default: false })
+  public logarithmic!: boolean;
 
   public get dates(): string[] {
-    return Object.keys(Object.values(this.deaths)[0]);
+    return Object.keys(Object.values(this.rootModule.getters.deaths)[0]);
   }
 
   public get dataSets(): Dataset[] {
-    const dataSets = transformCaseRecordsToDataset(this.deaths);
+    const dataSets = transformCaseRecordsToDataset(
+      this.rootModule.getters.deaths,
+    );
     let colors: string[];
     if (dataSets.length === 1) {
       colors = [COLORS.dead];
