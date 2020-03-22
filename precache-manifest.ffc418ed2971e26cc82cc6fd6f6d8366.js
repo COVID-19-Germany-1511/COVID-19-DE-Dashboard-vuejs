9690c4aba0481eefa2dfdf5403414733c6476341
micgro42 <mic.grosse@googlemail.com>
2020-03-22 09:54:30 +0000

    Deploying to gh-pages from  - 9548fb32f3d35e59d0037e2ea0682e4eab5a08a8 🚀

---
+++ precache-manifest.ffc418ed2971e26cc82cc6fd6f6d8366.js
@@ -0,0 +1,33 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2b0dcdea44f73bfcb5ed4847d9e87b",
+    "url": "index.html"
+  },
+  {
+    "revision": "80bd2e6bfac3c199337861896acc122b",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "ce0eface77d451fcad73",
+    "url": "static/css/app.3d894bb1.css"
+  },
+  {
+    "revision": "101411b57c3c100d60e7",
+    "url": "static/css/chunk-vendors.29192637.css"
+  },
+  {
+    "url": "static/img/svg-sprite.cd321ea4.svg"
+  },
+  {
+    "revision": "ce0eface77d451fcad73",
+    "url": "static/js/app.307cd615.js"
+  },
+  {
+    "revision": "101411b57c3c100d60e7",
+    "url": "static/js/chunk-vendors.53554ed2.js"
+  }
+]);
