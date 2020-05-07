e96edb7f3e42cd9f165bba42ee946a4c27c294f9
micgro42 <mic.grosse@googlemail.com>
2020-05-07 08:01:48 +0000

    Deploying to gh-pages from  @ 129c631158ea6f49515b9887d0b6630b7632c4ac 🚀

---
+++ precache-manifest.41fcf5848c1383b7b8bf141d83cedcb1.js
@@ -0,0 +1,37 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f94faba3bed9d96b0175ac7190c86e7",
+    "url": "index.html"
+  },
+  {
+    "revision": "80bd2e6bfac3c199337861896acc122b",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "12058277a68afe20eedd",
+    "url": "static/css/app.ddd4b3d1.css"
+  },
+  {
+    "revision": "68682b4afa63b1e35fd3",
+    "url": "static/css/chunk-vendors.29192637.css"
+  },
+  {
+    "revision": "cc5fdfadec5c4baaa850ef940e6ae70d",
+    "url": "static/img/select-arrow.cc5fdfad.svg"
+  },
+  {
+    "url": "static/img/svg-sprite.1ace1549.svg"
+  },
+  {
+    "revision": "12058277a68afe20eedd",
+    "url": "static/js/app.ec902683.js"
+  },
+  {
+    "revision": "68682b4afa63b1e35fd3",
+    "url": "static/js/chunk-vendors.c4e2228d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8f84819790d5b5fe308630735b7a7640.js"
+  "precache-manifest.41fcf5848c1383b7b8bf141d83cedcb1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "covid-19-de-dashboard-vuejs"});
